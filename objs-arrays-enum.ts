1477bb5b11416e540ef115fd5fcac968e1314c43
julia doroshina <yuspiri@gmail.com>
2024-03-25 12:14:35 +0100

    feat: add ts objs-arrays-enum

--- objs-arrays-enum.ts
+++ objs-arrays-enum.ts
@@ -1,3 +1,4 @@
+
 // const person: {
 //     name: string;
 //     age: number;
@@ -9,28 +10,28 @@
 //     hobbies: ['Sports', 'Cooking'],
 //     role: [2, 'author']
 // }
+
 //enum is a custom type
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
+enum Role { ADMIN, READ_ONLY, AUTHOR}
+const person = {
     name: "Yulia",
     age: 35,
     hobbies: ['Sports', 'Cooking'],
     role: Role.ADMIN
-};
+}
+
 // person.role.push('admin'); //push actually is an exception which is allowed in tuples.
-var favouriteActivities;
-favouriteActivities = ['Sports'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
+
+let favouriteActivities: string[];
+favouriteActivities = ['Sports']
+
+console.log(person.name)
+
+for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
     // console.log(hobby.map()); // !!! ERROR!!!
 }
+
 if (person.role === Role.ADMIN) {
-    console.log('is admin');
+    console.log('is admin')
 }
