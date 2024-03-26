308de52c2fc0b45e8a8ba89f942619a72520b8dd
julia doroshina <yuspiri@gmail.com>
2024-03-26 15:44:02 +0100

    feat: add union-alias types

--- app.js
+++ app.js
@@ -1,36 +1,22 @@
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string]; //tulip
-// } = {
-//     name: "Yulia",
-//     age: 35,
-//     hobbies: ['Sports', 'Cooking'],
-//     role: [2, 'author']
-// }
-//enum is a custom type
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: "Yulia",
-    age: 35,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-// person.role.push('admin'); //push actually is an exception which is allowed in tuples.
-var favouriteActivities;
-favouriteActivities = ['Sports'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map()); // !!! ERROR!!!
+function combine(input1, input2, resultConversion //literal types
+) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    //     if (resultConversion === 'as-number') {
+    //         return parseFloat(result);
+    //     } else {
+    //         return result.toString();
+    //     }
+    return result;
 }
-if (person.role === Role.ADMIN) {
-    console.log('is admin');
-}
+var combineAges = combine(30, 26, 'as-number');
+console.log(combineAges);
+var combineStringAges = combine('30', '26', 'as-number');
+console.log(combineStringAges);
+var combineNames = combine('Max', 'Anna', 'as-text');
+console.log(combineNames);
